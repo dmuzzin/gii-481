6bcaad525c0e55ec9e78c0372f91810d4356a3d7
Daniel Vukelich <daniel.c.vukelich@gmail.com>
2016-10-10 12:55:21 -0400

    Loops through and tries to find unidentified images

--- image_ident.js
+++ image_ident.js
@@ -1,1 +1,8 @@
-console.log("Page contains " + document.getElementsByTagName('img').length + " images");
+var images = document.getElementsByTagName('img');
+for(i = 0; i < images.length; ++i){
+    if(images[i].attr('alt')){
+	//Image contains alt-text.  We probably don't want to do anything
+    }else{
+	console.log("Page contains image: " + images[i].attr("src") + " that doesn't have alt-text")
+    }
+}
