b8da5f18462284b8b8e2fb6f03d7b04ed2332ebe
Daniel Vukelich <Daniel.C.Vukelich@gmail.com>
2016-10-10 14:51:41 -0400

    Plugin creates dialog box asking to transcribe images

--- image_ident.js
+++ image_ident.js
@@ -1,9 +1,20 @@
-var images = document.getElementsByTagName('img');
+var images = document.getElementsByTagName("img");
+var unnamed_count = 0;
 for(i = 0; i < images.length; ++i){
-    if(images[i].getAttribute('alt')){
-	//Image contains alt-text.  We probably don't want to do anything
-    }else{
-	console.log("Page contains image: " + images[i].getAttribute("src") + " that doesn't have alt-text")
-	//Now do something with the image
+    if(!images[i].getAttribute("alt")){
+	//Image has no alt-text
+	unnamed_count = unnamed_count + 1;
+    }
+}
+
+//If the page contains images without an alt-text, as the user if we should transcribe them
+if(unnamed_count != 0){
+    if(confirm("This page has " + unnamed_count + " images that your screen reader cannot read.  Would you like to try to transcribe them?")){
+	for(i = 0; i < images.length; ++i){
+	    if(!images[i].getAttribute("alt")){
+		//Image has no alt-text.  Send it to be transcribed
+		console.log(images[i].getAttribute("src") + " doesn't have alt-text")
+	    }
+	}	
     }
 }
