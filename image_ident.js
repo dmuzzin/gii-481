44b85ffb782c4e75d4907345135e755e4645b569
Daniel Vukelich <Daniel.C.Vukelich@gmail.com>
2016-10-10 14:26:40 -0400

    plugin detects images that don't have alt-text

--- image_ident.js
+++ image_ident.js
@@ -1,8 +1,9 @@
 var images = document.getElementsByTagName('img');
 for(i = 0; i < images.length; ++i){
-    if(images[i].attr('alt')){
+    if(images[i].getAttribute('alt')){
 	//Image contains alt-text.  We probably don't want to do anything
     }else{
-	console.log("Page contains image: " + images[i].attr("src") + " that doesn't have alt-text")
+	console.log("Page contains image: " + images[i].getAttribute("src") + " that doesn't have alt-text")
+	//Now do something with the image
     }
 }
