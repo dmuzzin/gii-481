69314ea2dddec2ad3db78bb7366ae6f844b8a1f4
William Howard <wipatric@umich.edu>
2016-11-09 09:06:02 -0500

    added a basic popup for proof of concept.

--- background.js
+++ background.js
@@ -1,1 +1,17 @@
 //alert("This page can runs in the backgrouna and could eventually be used for caching")
+chrome.runtime.onMessage.addListener(function(request) {
+    if (request.type === 'webpage_settings') {
+        chrome.tabs.create({
+            url: chrome.extension.getURL('popup.html'),
+            active: false
+        }, function(tab) {
+            // After the tab has been created, open a window to inject the tab
+            chrome.windows.create({
+                tabId: tab.id,
+                type: 'popup',
+                focused: true
+                // incognito, top, left, ...
+            });
+        });
+    }
+});
