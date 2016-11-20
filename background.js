857d54a7114c1c4b0c41b0d621256e154d4760bd
William Howard <wipatric@umich.edu>
2016-11-20 17:28:49 -0500

    settings savings working in image_ident.js. Message response from background.js to image_ident.j needs fixing

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 //alert("This page can runs in the backgrouna and could eventually be used for caching")
-chrome.runtime.onMessage.addListener(function(request) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendBack) {
 	//generate the html popup
     if (request.type === 'webpage_settings') {
         chrome.tabs.create({
@@ -15,32 +15,37 @@ chrome.runtime.onMessage.addListener(function(request) {
 				width:237
             });
         });
+	
+		//listen for the popup button press respons
+		chrome.runtime.onMessage.addListener(
+		function(request, sender, sendResponse) {
+		if (request.message == "saveTrans")
+		{
+			//do something to save settings as always transcribe
+			//chrome.storage.sync.set({ key: "save as transcribe" });
+			sendResponse({farewell: "saving as always transcribe"});
+		}
+		if(request.message == "saveNoTrans")
+		{
+			//do something to save settings as never transcribe
+			//chrome.storage.sync.set({ key: "save do not transcribe" });
+			sendResponse({farewell: "saving as never transcribe"});
+		}
+		if(request.message == "doTrans")
+		{
+			//do the transcription for the website
+			//chrome.storage.sync.set({ key: "do transcribe"})
+			sendResponse({farewell: "do the transcription"});
+		}
+		if(request.message == "noTrans")
+		{
+			//do nothing
+			//chrome.storage.sync.set({ key: "do not transcribe"})
+			sendResponse({farewell: "do not transcribe"});
+		}
+		});
+		//this sends a response back to image_ident
+		//the idea was that it send back what image_ident should do
+		sendBack({farewell: "pooper scooper"});
     }    
 });
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message == "saveTrans")
-	{
-		//do something to save settings as always transcribe
-		chrome.storage.sync.set({ key: "save transcribe" });
-		sendResponse({farewell: "saving as always transcribe"});
-	}
-	if(request.message == "saveNoTrans")
-	{
-		//do something to save settings as never transcribe
-		chrome.storage.sync.set({ key: "save do not transcribe" });
-		sendResponse({farewell: "saving as never transcribe"});
-	}
-	if(request.message == "doTrans")
-	{
-		//do the transcription for the website
-		chrome.storage.sync.set({ key: "do transcribe"})
-		sendResponse({farewell: "do the transcription"});
-	}
-	if(request.message == "noTrans")
-	{
-		chrome.storage.sync.set({ key: "do not transcribe"})
-		sendResponse({farewell: "do not transcribe"});
-	}
-  });
--- image_ident.js
+++ image_ident.js
@@ -8,42 +8,28 @@ for(i = 0; i < images.length; ++i){
     }
 }
 
+
 //If the page contains images without an alt-text, ask the user if we should transcribe them
 if(unnamed_count != 0){
-	//atempt to load settings
-	chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
-		console.log(message.text);
-	});
-	
-	//test function
+
+	//replace "dingydooo" with the setting to be saved
 	localStorage.setItem(document.domain,"dingydooo");
 	console.log(localStorage.getItem(document.domain));
 	
 	
 	//create popup window in background
-	chrome.runtime.sendMessage({type:'webpage_settings'});
-	
-	//read a key that was saved in background.js to determine something
-	var settings; 
-	var setit="nothing";
-	chrome.storage.sync.get('key', function(settings) { 
-		setit = settings.key;
+	chrome.runtime.sendMessage({type: 'webpage_settings'}, function(response) {
+		console.log(response.farewell); //prints backgrounds response 
 	});
-
-	console.log("items = " + setit); 
 	
-	//chrome.storage.sync.set({ key: "nothing"})
 	
-	//transcribe based on setting (not working)
 	/*
-	if(transcribe)
-	{
-		console.log("transcribe");
-	}
-	else
-	{
-		console.log("not transcribe");		
-	}*/
+	var obj;
+	chrome.storage.sync.get('key', function (obj) {
+    console.log('myKey', obj.key);
+    });
+	*/
+	
 /*
     if(confirm("This page has " + unnamed_count + " images that your screen reader cannot read.  Would you like to try to transcribe them?")){
 	//Add generic alt attribute to these images for Alpha release
