391d83f11ad6ac21f503a3296ae05318103534ae
William Howard <wipatric@umich.edu>
2016-11-21 09:31:56 -0500

    added settings saving by making additional dialog box. Also only generates boxes if there are images without alt text.

--- image_ident.js
+++ image_ident.js
@@ -1,4 +1,4 @@
-/* var images = document.getElementsByTagName("img");
+var images = document.getElementsByTagName("img");
 var unnamed_count = 0;
 for(i = 0; i < images.length; ++i){
     if(!images[i].getAttribute("alt")){
@@ -10,7 +10,8 @@ for(i = 0; i < images.length; ++i){
     }
 }
 
-
+/*
+//old testing code
 //If the page contains images without an alt-text, ask the user if we should transcribe them
 if(unnamed_count != 0){
 
@@ -47,36 +48,65 @@ if(unnamed_count != 0){
 	}
     }
 }*/
-
-if(confirm("This page has images that your screen reader cannot read. Would you like them all transcribed now? If not you can transcribe each image individually")) {
-	var images = document.getElementsByTagName("img");
-	for(i = 0; i < images.length; ++i){
-    	if(!images[i].getAttribute("alt")){
-			var replace = new Replacer();
-			replace.parseMicrosoftDescribe(images[i], 0);
-			replace.toDataUrl(images[i], 'OCR');
+if(unnamed_count !=0) {
+	//localStorage.clear();
+	var settings = localStorage.getItem(document.domain); //load settings if available (null if not saved)
+	console.log("settings loaded = " + settings)
+	if(settings == "transcribe") { //transcribe because it was saved to do so
+		var images = document.getElementsByTagName("img");
+		for(i = 0; i < images.length; ++i){
+			if(!images[i].getAttribute("alt")){
+				var replace = new Replacer();
+				replace.parseMicrosoftDescribe(images[i], 0);
+				replace.toDataUrl(images[i], 'OCR');
+			}
 		}
-    }
-} else {
-	var images = document.getElementsByTagName("img");
-	for(i = 0; i < images.length; ++i){
-    	if(!images[i].getAttribute("alt")){
-			//Image has no alt-text
-			images[i].tabIndex = 0;
+	}
+	else if(settings == "not transcribe") {
+		var images = document.getElementsByTagName("img");
+		for(i = 0; i < images.length; ++i){
+			if(!images[i].getAttribute("alt")){
+				//Image has no alt-text
+				images[i].tabIndex = 0;
+			}
 		}
-    }
-}
+	}
+	else if(confirm("This page has " + unnamed_count + " images that your screen reader cannot read. Would you like them all transcribed now? If not you can transcribe each image individually")) {
+		var images = document.getElementsByTagName("img");
+		for(i = 0; i < images.length; ++i){
+			if(!images[i].getAttribute("alt")){
+				var replace = new Replacer();
+				replace.parseMicrosoftDescribe(images[i], 0);
+				replace.toDataUrl(images[i], 'OCR');
+			}
+		}
+		if(confirm("Would you like to save this setting?")) { //save settings if confirmed to
+			localStorage.setItem(document.domain, "transcribe");
+		}
+	} else {
+		var images = document.getElementsByTagName("img");
+		for(i = 0; i < images.length; ++i){
+			if(!images[i].getAttribute("alt")){
+				//Image has no alt-text
+				images[i].tabIndex = 0;
+			}
+		}
+		if(confirm("Would you like to save this setting?")) { //save settings if confirmed to
+			localStorage.setItem(document.domain, "not transcribe");
+		}
+	}
 
-$('img').focus(function () {
-	if(this.alt == "" || !this.alt) {
-		//Hacky way to prevent confirm message from firing twice
-		this.alt = " ";
-		if(confirm("This is an image that your screen reader cannot read.  Would you like to try to transcribe it?")){
-			var replace = new Replacer();
-			replace.parseMicrosoftDescribe(this, 0);
-			replace.toDataUrl(this, 'OCR');
+	$('img').focus(function () {
+		if(this.alt == "" || !this.alt) {
+			//Hacky way to prevent confirm message from firing twice
+			this.alt = " ";
+			if(confirm("This is an image that your screen reader cannot read.  Would you like to try to transcribe it?")){
+				var replace = new Replacer();
+				replace.parseMicrosoftDescribe(this, 0);
+				replace.toDataUrl(this, 'OCR');
+				this.blur();
+			}
 			this.blur();
 		}
-		this.blur();
-	}
-})
+	})
+}
